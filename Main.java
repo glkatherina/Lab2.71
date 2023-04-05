b1230f114a0d5445a6b23e678e6c6013b8097acd
Gilca Ecaterina <126197139+glkatherina@users.noreply.github.com>
2023-04-05 19:18:16 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,9 +10,15 @@ public class Main {
      @param args аргументы командной строки
      */
     public static void main(String[] args) {
+         //Объявление и инициализация массива объектов класса Menu
+        //Создает массив из 50 объектов класса Menu
         Menu[] restaurantmenu = new Menu[50];
 
         try {
+        //Чтение данных из файла
+        //Создается объект FileReader, который открывает файл для чтения, и передается в конструктор BufferedReader.
+        //В цикле читаются данные из файла и сохраняются в каждом элементе массива restaurantmenu.
+            
             FileReader fileReader = new FileReader("D:\\УНИВЕР\\II курс\\ASDC\\Lab2.71\\src\\menu.txt");
             BufferedReader bufferedReader = new BufferedReader(fileReader);
 
@@ -26,8 +32,13 @@ public class Main {
             for (int i = 0; i < restaurantmenu.length; i++) {
                 restaurantmenu[i].writeTable(form);
             }
+        //Закрытие файла с помощью метода close() объекта BufferedReader
+
             bufferedReader.close();
-        } catch (IOException e) {
+        } 
+        //Обработка исключений
+
+catch (IOException e) {
             System.out.println("Error " + e.getMessage());
         }
     }
